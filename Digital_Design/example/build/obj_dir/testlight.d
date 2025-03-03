5f9c2958575ba6d784c560ad7f49394a920a7b09
nullstarfish <nullstarfish135@gmail.com>
2025-03-03 21:08:53 +0800

    完成了verilator的配置，以及第一次的nvboard接入，使用了example

---
+++ Digital_Design/example/build/obj_dir/testlight.d
@@ -0,0 +1,9 @@
+testlight.o: \
+ /home/nullstarfish/Desktop/NullStarfish-s-ysyxRepo/Digital_Design/example/csrc/testlight.cpp \
+ Vlight.h /usr/local/share/verilator/include/verilated.h \
+ /usr/local/share/verilator/include/verilatedos.h \
+ /usr/local/share/verilator/include/verilated_config.h \
+ /usr/local/share/verilator/include/verilated_types.h \
+ /usr/local/share/verilator/include/verilated_funcs.h \
+ /home/nullstarfish/Desktop/ysyx-workbench/nvboard/usr/include/nvboard.h \
+ /home/nullstarfish/Desktop/ysyx-workbench/nvboard/usr/include/pins.h
---
+++ Digital_Design/example/csrc/testlight.cpp
@@ -0,0 +1,49 @@
+#include <Vlight.h>
+#include "verilated.h"
+#include <nvboard.h>
+
+// 以下为伪代码
+VerilatedContext* contextp = NULL;
+static Vlight* top = new Vlight();
+
+void nvboard_bind_all_pins(Vlight* top);
+
+void single_cycle() {
+    top->clk = 0; 
+    top->eval();
+    top->clk = 1; 
+    top->eval();
+}
+
+void reset(int n) {
+    top->rst = 1;
+    while (n-- > 0) 
+        single_cycle();
+    top->rst = 0;
+}
+
+void sim_init() {
+    contextp = new VerilatedContext;
+    top = new Vlight;
+}
+
+void sim_exit() {
+    delete contextp;
+}
+
+
+int main() {
+    sim_init();
+    nvboard_bind_all_pins(top);
+    nvboard_init();
+
+
+    reset(10);  // 复位10个周期
+    while(!Verilated::gotFinish()) {
+        single_cycle();
+        nvboard_update();
+    }
+    sim_exit();
+    nvboard_quit();
+    return 0;
+}
